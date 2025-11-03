36e54b1378e156c0cea6d223995b46de106b2b5a
vilebile17 <ibrahimsomauroo@proton.me>
2025-11-03 08:06:58 +0000

    more cleaning...

--- eulerPi.go
+++ eulerPi.go
@@ -10,10 +10,10 @@ type EulerPi struct {
 }
 // Uses Leonhard Euler's formula: pi²/6 = 1/1² + 1/2² + 1/3² + 1/4² ...
 // This func calculates each fraction i.e. 1/i²
-func (e EulerPi) calculateNextVal(i int) float64 {
-	return float64(math.Pow(float64(i), -2.0))
+func (e *EulerPi) calculateNextVal(i int) {
+	e.sumSoFar += float64(math.Pow(float64(i), -2.0))
 }
 // Uses the sumSoFar value to calculate pi
-func (e EulerPi) calculatePi() float64 {
-	return math.Pow(e.sumSoFar * 6,0.5)
+func (e *EulerPi) calculatePi() {
+	e.val = math.Pow(e.sumSoFar * 6,0.5)
 }
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// for the cool printing effects...
-	color.Cyan("Calculating pi...")
+	color.Green("Calculating pi...")
 	fmt.Println("")
 
 	// creates the eulerPi struct
@@ -18,10 +18,10 @@ func main() {
 
 	// calculation loop
 	for i:=1; true ; i++ {
-		eulerPi.sumSoFar += eulerPi.calculateNextVal(i)
+		eulerPi.calculateNextVal(i)
 
 		if (i + 1) % 100000 == 0 {
-			eulerPi.val = eulerPi.calculatePi()
+			eulerPi.calculatePi()
 			fmt.Printf("\r===  ℼ = %.20f  ===", eulerPi.val)
 		}
 		
